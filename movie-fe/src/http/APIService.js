d3ab9b809d761c517c3d910ff1dbcc35b13a2602
kfritts0409 <kfritts09@gmail.com>
2024-09-29 17:53:04 -0400

    pointed the api service to python anywhere

--- movie-fe/src/http/APIService.js
+++ movie-fe/src/http/APIService.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 // Change the API_URL to the correct location of the backend API before deploying the app
 
 
-const API_URL = 'http://127.0.0.1:8000';/* 'http://localhost:8000' http://127.0.0.1:8000 or  'https://yourPythonAnywhereName.pythonanywhere.com'*/
+// const API_URL = 'http://127.0.0.1:8000';/* 'http://localhost:8000' http://127.0.0.1:8000 or  'https://yourPythonAnywhereName.pythonanywhere.com'*/
 
-// const API_URL = 'https://kfritts09.pythonanywhere.com'
+const API_URL = 'https://kfritts09.pythonanywhere.com'
 
 export class APIService {
   constructor() {
